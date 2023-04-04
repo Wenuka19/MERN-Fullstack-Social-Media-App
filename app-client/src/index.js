de46cfb0924ea9e04111042bd7a9e25cde3ca34f
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2023-04-04 11:49:33 +0530

    client initial setup

---
+++ app-client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+
+/**
+ * react-redux : state management tool
+ * redux-toolkit : wrapper around redux to make it easy to use redux
+ * redux-persist : store state in local storage
+ * react-dropzone : handle file upload on the front end
+ * dotenv : envri
+ * formik : form validation
+ * yup : validation
+ * react-router : routing
+ * MUI
+ */
