378364f612f516015f2edc7ac84d632103bce7d1
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2023-04-06 16:43:55 +0530

    route protection from the frontend

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -11,15 +11,16 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userid" element={<ProfilePage />} />
+            <Route path="/" element={isAuth ? <LoginPage /> : <Navigate to="/" />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+            <Route path="/profile/:userid" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
