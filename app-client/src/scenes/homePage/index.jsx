b44634c16e035341853d5864f4424f3d3d31d2f0
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2023-04-08 18:38:48 +0530

    ✅ PostsWidget, PostsWidget on frontend

--- app-client/src/scenes/homePage/index.jsx
+++ app-client/src/scenes/homePage/index.jsx
@@ -3,6 +3,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget"
+import PostsWidget from "scenes/widgets/PostsWidget";
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -23,6 +24,8 @@ const HomePage = () => {
                 <Box
                     flexBasis={isNonMobileScreens ? "42%" : undefined} mt={isNonMobileScreens ? undefined : "2rem"}>
                     <MyPostWidget picturePath={picturePath} />
+
+                    <PostsWidget userId={_id} />
                 </Box>
                 {isNonMobileScreens && (
                     <Box flexBasis="26%"></Box>
